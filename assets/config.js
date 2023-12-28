72df0d587437c15fda6c71978fa1f9dc13f09b26
vishruth <vishruth555@gmail.com>
2023-12-28 19:40:36 +0530

    inital commit

---
+++ assets/config.js
@@ -0,0 +1,43 @@
+let config = {
+  "Title": "TEDx NHCE",
+  "Name": "",
+  "Description": " = independently organized TED event",
+  "links": [
+    {
+      "Title": "Register",
+      "URL": "",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Become a Sponsor",
+      "URL": "",
+      "icon_classes": "fab fa-github",
+    },
+    // {
+    //   "Title": "Play Store",
+    //   "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
+    //   "icon_classes": "fab fa-google-play"
+    // },
+    // {
+    // {
+    //   "Title": "Twitter",
+    //   "URL": "https://twitter.com/Harindu_Fonseka",
+    //   "icon_classes": "fab fa-twitter"
+    // },
+    {
+      "Title": "Instagram",
+      "URL": "https://www.linkedin.com/showcase/tedxofficial/",
+      "icon_classes": "fab fa-instagram"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/showcase/tedxofficial/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      "icon_classes": "fab fa-youtube"
+    },
+  ]
+}
